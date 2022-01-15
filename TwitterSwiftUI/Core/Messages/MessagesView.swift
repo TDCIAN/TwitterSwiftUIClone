255efd21752e69e5ee0e4c17b6e1e094a0a1d1bc
TDCIAN <tdcian71@gmail.com>
2022-01-15 12:01:06 +0900

    make TabView

---
+++ TwitterSwiftUI/Core/Messages/MessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesView.swift
+//  TwitterSwiftUI
+//
+//  Created by JeongminKim on 2022/01/15.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text("Messages")
+    }
+}
+
+struct MessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+    }
+}
