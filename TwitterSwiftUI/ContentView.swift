786b4c9c5f646a250e6eabd6f6250053a6404bd8
TDCIAN <tdcian71@gmail.com>
2022-01-15 11:28:40 +0900

    first commit

---
+++ TwitterSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUI
+//
+//  Created by JeongminKim on 2022/01/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
