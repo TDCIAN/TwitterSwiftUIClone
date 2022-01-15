033f9801b56d6375f10d75a21c2ec0686ca1283c
TDCIAN <tdcian71@gmail.com>
2022-01-15 16:04:01 +0900

    make sidemenu

---
+++ TwitterSwiftUI/Core/SideMenu/Views/SideMenuView.swift
@@ -0,0 +1,55 @@
+//
+//  SideMenuView.swift
+//  TwitterSwiftUI
+//
+//  Created by JeongminKim on 2022/01/15.
+//
+
+import SwiftUI
+
+struct SideMenuView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 32) {
+            VStack(alignment: .leading) {
+                Circle()
+                    .frame(width: 48, height: 48)
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Bruce Wayne")
+                        .font(.headline)
+                    
+                    Text("@batman")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                
+                UserStatsView()
+                    .padding(.vertical)
+            }
+            .padding(.leading)
+            
+            ForEach(SideMenuViewModel.allCases, id: \.rawValue) { viewModel in
+                if viewModel == .profile {
+                    NavigationLink(
+                        destination: {
+                            ProfileView()
+                        }, label: {
+                            SideMenuOptionRowView(viewModel: viewModel)
+                        }
+                    )
+                } else if viewModel == .logout {
+                    
+                }
+            }
+
+            Spacer()
+        }
+    }
+}
+
+struct SideMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuView()
+    }
+}
+
