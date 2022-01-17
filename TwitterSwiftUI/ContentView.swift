cb25100c731ee2832d98991d609a073a8f93ab5d
TDCIAN <tdcian71@gmail.com>
2022-01-18 08:38:30 +0900

    user sign in

--- TwitterSwiftUI/ContentView.swift
+++ TwitterSwiftUI/ContentView.swift
@@ -33,46 +33,44 @@ struct ContentView_Previews: PreviewProvider {
 extension ContentView {
     var mainInterfaceView: some View {
         Group {
-            NavigationView {
-                ZStack(alignment: .topLeading) {
-                    MainTabView()
-                        .navigationBarHidden(showMenu)
-                    
-                    if showMenu {
-                        ZStack {
-                            Color(.black)
-                                .opacity(showMenu ? 0.25 : 0.0)
-                        }
-                        .onTapGesture {
-                            withAnimation(.easeInOut) {
-                                showMenu = false
-                            }
-                        }
-                        .ignoresSafeArea()
+            ZStack(alignment: .topLeading) {
+                MainTabView()
+                    .navigationBarHidden(showMenu)
+                
+                if showMenu {
+                    ZStack {
+                        Color(.black)
+                            .opacity(showMenu ? 0.25 : 0.0)
                     }
-                    
-                    SideMenuView()
-                        .frame(width: 300)
-                        .offset(x: showMenu ? 0 : -300, y: 0)
-                        .background(showMenu ? Color.white : Color.clear)
-                }
-                .navigationTitle("Home")
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Button(action: {
-                            withAnimation(.easeInOut) {
-                                self.showMenu.toggle()
-                            }
-                        }, label: {
-                            Circle()
-                                .frame(width: 32, height: 32)
-                        })
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
+                            showMenu = false
+                        }
                     }
+                    .ignoresSafeArea()
                 }
-                .onAppear {
-                    self.showMenu = false
+                
+                SideMenuView()
+                    .frame(width: 300)
+                    .offset(x: showMenu ? 0 : -300, y: 0)
+                    .background(showMenu ? Color.white : Color.clear)
+            }
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        withAnimation(.easeInOut) {
+                            self.showMenu.toggle()
+                        }
+                    }, label: {
+                        Circle()
+                            .frame(width: 32, height: 32)
+                    })
                 }
+            }
+            .onAppear {
+                self.showMenu = false
             }
         }
     }
--- TwitterSwiftUI/Core/Authentication/ViewModels/AuthViewModel.swift
+++ TwitterSwiftUI/Core/Authentication/ViewModels/AuthViewModel.swift
@@ -19,6 +19,16 @@ class AuthViewModel: ObservableObject {
     
     func login(withEmail email: String, password: String) {
         print("DEBUG: Login with email \(email)")
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("DEBUG: Failed to sign in with error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let user = result?.user else { return }
+            self.userSession = user
+            print("DEBUG: Did log user in")
+        }
     }
     
     func register(withEmail email: String, password: String, fullname: String, username: String) {
@@ -34,6 +44,27 @@ class AuthViewModel: ObservableObject {
             
             print("DEBUG: Registered user successfully")
             print("DEBUG: User is \(user)")
+            
+            let data = [
+                "email": email,
+                "username": username.lowercased(),
+                "fullname": fullname,
+                "uid": user.uid
+            ]
+            
+            Firestore.firestore().collection("users")
+                .document(user.uid)
+                .setData(data) { _ in
+                    print("DEBUG: Did upload user data")
+                }
         }
+    }
+    
+    func signOut() {
+        // sets user session to nil so we show login view
+        userSession = nil
+        
+        // signs user out on server
+        try? Auth.auth().signOut()
     }
 }
